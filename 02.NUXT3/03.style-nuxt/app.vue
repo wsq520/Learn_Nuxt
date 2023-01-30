6f0aa306322b9d7788fe0309fd8bef6da240e9a6
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-01-30 23:43:39 +0800

    访问assets和public资源

--- 02.NUXT3/03.style-nuxt/app.vue
+++ 02.NUXT3/03.style-nuxt/app.vue
@@ -3,16 +3,29 @@
     <h1 class="global-style">Nuxt-APP</h1>
     <h1 class="global-style1">全局样式-main.css</h1>
     <h1 class="global-style2">全局样式-global.sass</h1>
+
+    <!-- 访问Public资源 -->
+    <img src="/js01.webp" alt="">
+
+    <!-- 访问assets资源 -->
+    <img class="a-img" src="@/assets/images/beauty.jpg" alt="">
+
+    <img class="a-img" :src="ImgOne" alt="">
     <NuxtPage></NuxtPage>
   </div>
 </template>
 
 <script setup>
-
+import ImgOne from '@/assets/images/beauty.jpg'
 </script>
 
 <style>
 .global-style {
   color: red
+}
+
+.a-img {
+  width: 200px;
+  height: 180px;
 }
 </style>
