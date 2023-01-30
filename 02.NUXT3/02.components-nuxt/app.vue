7da403be168d5e20b1dd1bdb7930e8e1540a16dc
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-01-30 22:18:10 +0800

    ClientOnly组件的使用

---
+++ 02.NUXT3/02.components-nuxt/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Nuxt-APP</h1>
+    <ClientOnly fallback-tag="h1" fallback="loading....">
+      我只在客户端渲染 服务器返回内容没我 不信你看看
+    </ClientOnly>
+    <NuxtPage></NuxtPage>
+    <ClientOnly>
+      <div>我在客户端渲染</div>
+      <template #fallback>
+        <div>
+          我是服务端渲染的loading...
+        </div>
+      </template>
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup>
+
+</script>
