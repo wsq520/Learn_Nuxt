a3bdc4687e6465237ba95447935009341c607be9
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-02-02 16:23:14 +0800

    页面布局的使用

---
+++ 02.NUXT3/07.layout-nuxt/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="layout">
+    <div class="header">
+      <h1>我是header</h1>
+      <div>
+        <NuxtLink to="/">
+          <button>首页</button>
+        </NuxtLink>
+        <NuxtLink to="/cart">
+          <button>购物车</button>
+        </NuxtLink>
+        <NuxtLink to="/category">
+          <button>分类</button>
+        </NuxtLink>
+        <NuxtLink to="/profile">
+          <button>个人</button>
+        </NuxtLink>
+        <NuxtLink to="/login">
+          <button>登录</button>
+        </NuxtLink>
+      </div>
+    </div>
+    <slot></slot>
+    <div class="footer">我是footer</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.header {
+  background-color: red;
+  text-align: center;
+  font-size: 30px;
+}
+
+.footer {
+  background-color: blue;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
---
+++ 02.NUXT3/07.layout-nuxt/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>login页面使用专属布局</p>
+    Page: Login
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'custom-layout'
+})
+</script>
+
+<style scoped>
+
+</style>
