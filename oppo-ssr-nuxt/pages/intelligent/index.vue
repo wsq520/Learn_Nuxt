3dc609d17000ab5b9717d84bf82ab38bbf313a52
Coderwsq <9699507+wsq__666@user.noreply.gitee.com>
2023-03-11 11:45:22 +0800

    实现oneplus\硬件专区页面

--- oppo-ssr-nuxt/pages/intelligent/index.vue
+++ oppo-ssr-nuxt/pages/intelligent/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="intelligent">
-    intelligent
+    <div class="wrapper content">
+      <swiper :banners="data?.data.banners"></swiper>
+      <tab-category :categorys="data?.data.categorys"></tab-category>
+      <template v-for="category in data?.data.categorys" :key="category.id">
+        <section-category
+          :category="category"
+          v-if="category.productDetailss && category.productDetailss.length"
+        ></section-category>
+      </template>
+    </div>
   </div>
 </template>
-
 <script lang="ts" setup>
-// export interface IProps {
-//   title: string
-// }
-
-// const props = withDefaults(defineProps<IProps>(), {
-//   title: ''
-// })
+import { getHomeInfo } from '@/service/home'
+const { data } = await getHomeInfo('intelligent')
 </script>
 
-<style lang="scss">
-.intelligent {
-
-}
+<style lang="scss" scoped>
 </style>
--- oppo-ssr-nuxt/pages/onePlus/index.vue
+++ oppo-ssr-nuxt/pages/onePlus/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="onePlus">
-    onePlus
+    <div class="wrapper content">
+      <swiper :banners="data?.data.banners"></swiper>
+      <tab-category :categorys="data?.data.categorys"></tab-category>
+      <template v-for="category in data?.data.categorys" :key="category.id">
+        <section-category
+          :category="category"
+          v-if="category.productDetailss && category.productDetailss.length"
+        ></section-category>
+      </template>
+    </div>
   </div>
 </template>
-
 <script lang="ts" setup>
-// export interface IProps {
-//   title: string
-// }
-
-// const props = withDefaults(defineProps<IProps>(), {
-//   title: ''
-// })
+import { getHomeInfo } from '@/service/home'
+const { data } = await getHomeInfo("onePlus")
 </script>
 
-<style lang="scss">
-.onePlus {
-
-}
+<style lang="scss" scoped>
 </style>
